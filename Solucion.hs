2bcf4d31f6b8f541f6faa29d42a1ca33ee33b4f5
Natalia Perez <nataliaperestrepo@gmail.com>
2024-05-22 14:00:08 -0300

    archivo con todos los ejercicios

--- Solucion.hs
+++ Solucion.hs
@@ -16,8 +16,6 @@ import Data.Char
 
 
 -- Las letras en minuscula van de la 97 a la 122
--- Las mayusculas van de la 65 a la 90
---Cuando una variable es llamada palabra, hace referencia a una palabra o a una frase
 
 --PARTE I
 -- EJ 1
@@ -38,122 +36,135 @@ desplazar caracter movimiento | esMinuscula caracter == True && (ord(caracter) +
 desplazarAux :: Char -> Int -> Char
 desplazarAux caracter movimiento | movimiento>0 && (ord(caracter) + movimiento)>122 = desplazarAux caracter (movimiento-26)
                                  | movimiento<0 && (ord(caracter) + movimiento)<97 = desplazarAux caracter (movimiento+26)
-                                 |otherwise = chr(ord(caracter) + movimiento)
+                                 | otherwise = chr(ord(caracter) + movimiento)
 
 -- EJ 4
 cifrar :: String -> Int -> String
 cifrar "" _ = ""
-cifrar (x:xs) movimiento = (desplazar x movimiento) : (cifrar xs movimiento) --Donde x es el primer Char y xs el resto de Chars de la palabra o frase
+cifrar (c:cs) movimiento = (desplazar c movimiento) : (cifrar cs movimiento) 
 
 -- EJ 5
 descifrar :: String -> Int -> String
 descifrar "" _ = ""
-descifrar (x:xs) movimiento = (desplazar x (-movimiento)) : (descifrar xs movimiento) --Donde x es el primer Char y xs el resto de Chars de la palabra o frase
+descifrar (c:cs) movimiento = (desplazar c (-movimiento)) : (descifrar cs movimiento)
 
 -- EJ 6
 cifrarLista :: [String] -> [String]
 cifrarLista [] = []
-cifrarLista lista = cifrarListaAux lista 0 --El primer elemento tiene posición cero, por lo tanto, su desplazamiento será cero
+cifrarLista lista = cifrarListaAux lista 0 
+            --el primer elemento tiene posicion cero, por lo tanto, su desplazamiento sera cero
 
 cifrarListaAux :: [String] -> Int -> [String]
 cifrarListaAux [] _= []
-cifrarListaAux (palabra:xs) movimiento = (cifrar palabra movimiento) : cifrarListaAux xs (movimiento+1) --xs es la lista de palabras (o frases) sin incluir la primera
+cifrarListaAux (msj:msjs) movimiento = (cifrar msj movimiento) : cifrarListaAux msjs (movimiento+1) 
 
 -- EJ 7
 frecuencia :: String -> [Float]
 frecuencia "" = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-frecuencia palabra |totalMinusculas palabra == 0 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-                   |otherwise = frecuenciaAux palabra 'a'
+frecuencia msj | totalMinusculas msj == 0 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+               | otherwise = frecuenciaAux msj 'a'
+               --la primera posicion de la lista resultado corresponde a la letra 'a'
 
 frecuenciaAux :: String -> Char -> [Float]
-frecuenciaAux palabra letra |ord(letra)>122 = [] --Las letras en minuscula van de la 97 a la 122 y empecé por 'a', que es 97, entonces termino en 122
-                            |otherwise = frecuenciaLetra : frecuenciaAux palabra (chr(ord(letra) + 1)) --hago recursion con la misma palabra y la siguiente letra del abecedario
-                            where frecuenciaLetra = fromInteger((aparicionesLetra letra palabra)*100) / fromInteger(totalMinusculas palabra)
+frecuenciaAux msj letra | ord(letra)>122 = [] --las letras en minuscula van de la 97 a la 122 y empecé por 'a', que es 97, entonces termino en 122
+                        | otherwise = frecuenciaLetra : frecuenciaAux msj (chr(ord(letra) + 1)) --hago recursion con el mismo mensaje y la siguiente letra minuscula del abecedario
+                        where frecuenciaLetra = fromInteger((aparicionesLetra letra msj)*100) / fromInteger(totalMinusculas msj)
 
-aparicionesLetra :: Char -> String -> Integer --funcion que devuelve cantidad de veces que aparece una letra en una palabra o frase
+aparicionesLetra :: Char -> String -> Integer --funcion que devuelve cantidad de veces que aparece una letra minuscula en un mensaje
 aparicionesLetra _ [] = 0
-aparicionesLetra letra (x:xs) |letra==x = 1 + aparicionesLetra letra xs --Donde x es el primer Char y xs el resto de Chars de la palabra o frase
-                              |otherwise = aparicionesLetra letra xs
+aparicionesLetra letra (c:cs) | letra==c = 1 + aparicionesLetra letra cs 
+                              | otherwise = aparicionesLetra letra cs
 
-totalMinusculas :: String -> Integer --funcion que devuelve la cantidad de minusculas en una palabra o frase
+totalMinusculas :: String -> Integer --funcion que devuelve la cantidad de minusculas en un mensaje
 totalMinusculas [] = 0
-totalMinusculas (x:xs) |esMinuscula x = 1 + totalMinusculas xs --Donde x es el primer Char y xs el resto de Chars de la palabra o frase
-                       |otherwise = totalMinusculas xs
+totalMinusculas (c:cs) | esMinuscula c = 1 + totalMinusculas cs 
+                       | otherwise = totalMinusculas cs
 
 -- Ej 8
 cifradoMasFrecuente :: String -> Int -> (Char, Float)
-cifradoMasFrecuente palabraACifrar movimiento = cifradoMasFrecuenteAux (frecuencia(cifrar palabraACifrar movimiento)) 97 --Para empezar con la 'a' usamos el numero 97
+cifradoMasFrecuente msj movimiento = cifradoMasFrecuenteAux (frecuencia(cifrar msj movimiento)) 97 
+                                                           --para empezar con la 'a' usamos el numero 97
 
 cifradoMasFrecuenteAux :: [Float] -> Int -> (Char, Float)
-cifradoMasFrecuenteAux [x] numeroLetra = ((chr(numeroLetra)) , x) --x es el unico porcentaje de la lista
-cifradoMasFrecuenteAux (porcentaje:xs) numeroLetra | porcentaje >= mayorPorcentaje = (chr(numeroLetra) , porcentaje) --xs es el resto de la lista
-                                                   | otherwise = cifradoMasFrecuenteAux xs (numeroLetra+1)
-                                                   where mayorPorcentaje = snd(cifradoMasFrecuenteAux xs (numeroLetra+1))
---El 8 funciona bien pero se tarda un poco
+cifradoMasFrecuenteAux [p] n = (chr(n) , p) 
+cifradoMasFrecuenteAux (p:ps) n | p >= mayorPorcentaje = (chr(n) , p) 
+                                | otherwise = cifradoMasFrecuenteAux ps (n+1)
+                                where mayorPorcentaje = snd(cifradoMasFrecuenteAux ps (n+1))
+--la p hace referencia a porcentaje y la n al número que representa a la letra
+--el 8 funciona bien pero se tarda un poco 
 
 -- EJ 9
 esDescifrado :: String -> String -> Bool
-esDescifrado palabraNormal palabraCapazCifrada = esDescifradoAux palabraNormal palabraCapazCifrada 0
+esDescifrado msj msjCapazCifrado = esDescifradoAux msj msjCapazCifrado 0
 
 esDescifradoAux :: String -> String -> Int -> Bool
-esDescifradoAux _ _ 26 = False -- Si dio la vuelta entera en la calesita del abecedario, no es descifrado
-esDescifradoAux palabraNormal palabraCapazCifrada n | cifrar palabraNormal n == palabraCapazCifrada = True 
-                                                    | otherwise = esDescifradoAux palabraNormal palabraCapazCifrada (n+1)
+esDescifradoAux _ _ 26 = False -- si dio la vuelta entera en la calesita del abecedario, no es descifrado
+esDescifradoAux msj msjCapazCifrado i | cifrar msj i == msjCapazCifrado = True 
+                                      | otherwise = esDescifradoAux msj msjCapazCifrado (i+1)
+
 -- EJ 10            
 todosLosDescifrados :: [String] -> [(String, String)]
-todosLosDescifrados listaPalabras = todosLosDescifradosAux listaPalabras listaPalabras 
+todosLosDescifrados lista = todosLosDescifradosAux lista lista
 
 todosLosDescifradosAux :: [String] -> [String] -> [(String, String)]
 todosLosDescifradosAux [] _ = []
-todosLosDescifradosAux (palabra:xs) listaPalabras = (descifrados palabra listaPalabras) ++ (todosLosDescifradosAux xs listaPalabras)
---xs es la lista de palabras (o frases) sin incluir a la primera
-descifrados :: String -> [String] -> [(String, String)]
-descifrados _ [] = []
-descifrados palabra1 (palabra2:xs) |esDescifradoAux palabra1 palabra2 1 = (palabra1, palabra2):(descifrados palabra1 xs)
-                                   |otherwise = descifrados palabra1 xs
---esta funcion arma pares donde el primer componente es palabra1 y el segundo componente son todas las palabras de la lista que son el resultado de cifrar(palabra1,n) para algun n
+todosLosDescifradosAux (msj:msjs) lista = (buscarDescifrados msj lista) ++ (todosLosDescifradosAux msjs lista)
+
+buscarDescifrados :: String -> [String] -> [(String, String)]
+buscarDescifrados _ [] = []
+buscarDescifrados mensaje (msj:msjs) | esDescifradoAux mensaje msj 1 = (mensaje, msj):(buscarDescifrados mensaje msjs)
+                                     | otherwise = buscarDescifrados mensaje msjs
+--esta funcion arma pares donde el primer componente es el mensaje y el segundo componente son todos los mensajes de la lista que son el resultado de cifrar(mensaje,n) para algun n cuyo mod 26 es distinto de 0
 
 --PARTE II
 -- EJ 11
 expandirClave :: String -> Int -> String
-expandirClave clave longitud =  expandirClaveAux clave clave [] longitud
+expandirClave clave longitud = expandirClaveAux clave clave [] longitud
 
 expandirClaveAux :: String -> String -> String -> Int -> String
-expandirClaveAux clave [x] claveExpandida longitud |longitud==length(claveExpandida)=claveExpandida
-                                                   |otherwise = expandirClaveAux clave clave (claveExpandida++[x]) longitud
-expandirClaveAux clave (x:xs) claveExpandida longitud |longitud==length(claveExpandida)=claveExpandida
-                                                      |otherwise = expandirClaveAux clave xs (claveExpandida++[x]) longitud
---Donde x es el primer Char de clave y xs el resto de Chars de clave
+expandirClaveAux clave [c] claveExpandida longitud | longitud == length(claveExpandida) = claveExpandida
+                                                   | otherwise = expandirClaveAux clave clave (claveExpandida++[c]) longitud
+expandirClaveAux clave (c:cs) claveExpandida longitud | longitud == length(claveExpandida) = claveExpandida
+                                                      | otherwise = expandirClaveAux clave cs (claveExpandida++[c]) longitud
 
 -- EJ 12
 cifrarVigenere :: String -> String -> String
-cifrarVigenere palabra clave = cifrarVigenereAux palabra (expandirClave clave (length(palabra)))
+cifrarVigenere msj clave = cifrarVigenereAux msj (expandirClave clave (length(msj)))
 
 cifrarVigenereAux :: String -> String -> String
 cifrarVigenereAux "" _ = ""
-cifrarVigenereAux (letra:xs) (letraClave:ys) = (desplazar letra (letraANatural letraClave)) : (cifrarVigenereAux xs ys)
+cifrarVigenereAux (c:cs) (cc:ccs) = (desplazar c (letraANatural cc)) : (cifrarVigenereAux cs ccs)
+--c hace referencia a un caracter del mensaje y cc a un caracter de la clave expandida
 
 -- EJ 13
 descifrarVigenere :: String -> String -> String
-descifrarVigenere palabra clave = descifrarVigenereAux palabra (expandirClave clave (length(palabra)))
+descifrarVigenere msj clave = descifrarVigenereAux msj (expandirClave clave (length(msj)))
 
 descifrarVigenereAux :: String -> String -> String
 descifrarVigenereAux "" _ = ""
-descifrarVigenereAux (letra:xs) (letraClave:ys) = (desplazar letra (-(letraANatural letraClave))) : (descifrarVigenereAux xs ys)
+descifrarVigenereAux (c:cs) (cc:ccs) = (desplazar c (-(letraANatural cc))) : (descifrarVigenereAux cs ccs)
+--c hace referencia a un caracter del mensaje y cc a un caracter de la clave expandida
 
 -- EJ 14
 peorCifrado :: String -> [String] -> String
-peorCifrado _ [x] = x
-peorCifrado mensaje (clave:xs) |distanciaConClave <= distanciaConPeorClave = clave
-                               |otherwise = peorCifrado mensaje xs
-                               where distanciaConClave = distancia (cifrarVigenere mensaje clave) mensaje (length mensaje)
-                                     distanciaConPeorClave = distancia (cifrarVigenere mensaje (peorCifrado mensaje xs)) mensaje (length mensaje)
+peorCifrado _ [clave] = clave
+peorCifrado mensaje (clave:claves) | distanciaConClave <= distanciaConPeorClave = clave
+                                   | otherwise = peorCifrado mensaje claves
+                                   where distanciaConClave = distancia (cifrarVigenere mensaje clave) mensaje (length(mensaje))
+                                         distanciaConPeorClave = distancia (cifrarVigenere mensaje (peorCifrado mensaje claves)) mensaje (length(mensaje))
 
-distancia :: String -> String -> Int -> Int
+distancia :: String -> String -> Int -> Int --funcion que calcula la distancia entre dos listas
 distancia _ _ 0 = 0
-distancia (letra1:mensaje1) (letra2:mensaje2) longitud = abs(letraANatural(letra1)-letraANatural(letra2))+distancia mensaje1 mensaje2 (longitud-1)
-
+distancia (cc:ccs) (c:cs) longitud = abs(letraANatural(cc)-letraANatural(c)) + distancia ccs cs (longitud-1)
+--c hace referencia a un caracter del mensaje y cc a un caracter de la clave expandida
 
 -- EJ 15
 combinacionesVigenere :: [String] -> [String] -> String -> [(String, String)]
-combinacionesVigenere _ _ _ = [("hola", "b")]
+combinacionesVigenere [] _ _ = [] 
+combinacionesVigenere (msj:msjs) claves cifrado = (combinacionesVigenereAux msj claves cifrado)++(combinacionesVigenere msjs claves cifrado)
+
+combinacionesVigenereAux :: String -> [String] -> String -> [(String, String)]
+combinacionesVigenereAux _ [] _ = []
+combinacionesVigenereAux msj (clave:claves) cifrado | (cifrarVigenere msj clave) == cifrado = (msj, clave):(combinacionesVigenereAux msj claves cifrado)
+                                                    | otherwise = combinacionesVigenereAux msj claves cifrado
+
